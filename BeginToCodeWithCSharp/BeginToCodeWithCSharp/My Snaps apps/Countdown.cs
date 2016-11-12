efe9516c995edb7d37b52ef3b04ba0eafa636fc6
Dean Friedland <dfmmalaw@gmail.com>
2016-11-12 13:13:01 -0500

    initial commit for book solution

---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Countdown.cs
@@ -0,0 +1,33 @@
+﻿using SnapsLibrary;
+
+public class Countdown
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Countdown");
+        SnapsEngine.DisplayString("10");
+        SnapsEngine.SpeakString("10");
+        SnapsEngine.DisplayString("9");
+        SnapsEngine.SpeakString("9");
+        SnapsEngine.DisplayString("8");
+        SnapsEngine.SpeakString("8");
+        SnapsEngine.DisplayString("7");
+        SnapsEngine.SpeakString("7");
+        SnapsEngine.DisplayString("6");
+        SnapsEngine.SpeakString("6");
+        SnapsEngine.DisplayString("5");
+        SnapsEngine.SpeakString("5");
+        SnapsEngine.DisplayString("4");
+        SnapsEngine.SpeakString("4");
+        SnapsEngine.DisplayString("3");
+        SnapsEngine.SpeakString("3");
+        SnapsEngine.DisplayString("2");
+        SnapsEngine.SpeakString("2");
+        SnapsEngine.DisplayString("1");
+        SnapsEngine.SpeakString("1");
+        SnapsEngine.DisplayString("0");
+        SnapsEngine.SpeakString("0");
+    }
+}
+
+
---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/EggTimer.cs
@@ -0,0 +1,12 @@
+﻿using SnapsLibrary;
+
+class EggTimer
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Egg Timer");
+        SnapsEngine.DisplayString("There are five minutes left");
+        SnapsEngine.Delay(300);
+        SnapsEngine.DisplayString("There are four minutes left");
+    }
+}
