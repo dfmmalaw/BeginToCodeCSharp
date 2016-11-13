99389826c54365a54e92ad90a2b23f59b77a3810
Dean Friedland <dfmmalaw@gmail.com>
2016-11-12 21:25:12 -0500

    Created another Egg Timer

--- BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/EggTimer.cs
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/EggTimer.cs
@@ -5,18 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BeginToCodeWithCSharp.My_Snaps_apps
+public class EggTimer
 {
-    class EggTimer
+    public void StartProgram()
     {
-        public void StartProgram()
-        {
-            SnapsEngine.SetTitleString("Egg Timer");
-            SnapsEngine.DisplayString("There are five minutes left");
-            SnapsEngine.Delay(300);
-            SnapsEngine.DisplayString("Your egg is done!");
-        }
+        SnapsEngine.SetTitleString("Egg Timer");
+        SnapsEngine.DisplayString("There are five minutes left");
+        SnapsEngine.Delay(300);
+        SnapsEngine.DisplayString("Your egg is done!");
     }
 }
+
 
 
