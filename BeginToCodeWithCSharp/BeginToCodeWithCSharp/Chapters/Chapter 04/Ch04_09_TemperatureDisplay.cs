ec8b8a4fa5470f98ee3e92afa1edc1de567b6a06
Dean Friedland <dfmmalaw@gmail.com>
2016-11-19 11:38:38 -0500

    Created a workout HIIT timer

--- BeginToCodeWithCSharp/BeginToCodeWithCSharp/Chapters/Chapter 04/Ch04_09_TemperatureDisplay.cs
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/Chapters/Chapter 04/Ch04_09_TemperatureDisplay.cs
@@ -5,7 +5,7 @@ class Ch04_09_TemperatureDisplay
     public void StartProgram()
     {
 int temperature =
-    SnapsEngine.GetTodayTemperatureInFahrenheit(latitude: 47.61, longitude: -122.33);
+    SnapsEngine.GetTodayTemperatureInFahrenheit(latitude: 34.03, longitude: -84.62);
         string fullMessage;
         fullMessage = "The current temperature is " + temperature + " degrees.";
         SnapsEngine.DisplayString(fullMessage);
---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/HiitTimer.cs
@@ -0,0 +1,27 @@
+﻿using SnapsLibrary;
+public class HiitTimer
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Time to man up!");
+        SnapsEngine.DisplayString("Five minutes to rock. Punch!!!!");
+        SnapsEngine.Delay(30);
+        SnapsEngine.DisplayString("Active rest!");
+        SnapsEngine.Delay(90);
+        SnapsEngine.DisplayString("Four minutes to live. Burpees!!!!");
+        SnapsEngine.Delay(30);
+        SnapsEngine.DisplayString("Active rest!");
+        SnapsEngine.Delay(90);
+        SnapsEngine.DisplayString("Three minutes of battle left. Knees!!!!");
+        SnapsEngine.Delay(30);
+        SnapsEngine.DisplayString("Active rest!");
+        SnapsEngine.Delay(90);
+        SnapsEngine.DisplayString("Two more minutes of fire. Get up and jump lunges!!!!");
+        SnapsEngine.Delay(30);
+        SnapsEngine.DisplayString("Active rest!");
+        SnapsEngine.Delay(90);
+        SnapsEngine.DisplayString("One more round. Burpees!!!!");
+        SnapsEngine.Delay(30);
+        SnapsEngine.DisplayString("You survived five minutes of hell");
+    }
+}
