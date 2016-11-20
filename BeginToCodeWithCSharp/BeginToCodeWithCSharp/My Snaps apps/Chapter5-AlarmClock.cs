69bf8e44611d4fb33b40c2d04a65001c0f9ab5fc
Dean Friedland <dfmmalaw@gmail.com>
2016-11-20 14:42:40 -0500

    built alarm clock

---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-AlarmClock.cs
@@ -0,0 +1,23 @@
+﻿using SnapsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Chapter5_AlarmClock
+{
+    public void StartProgram()
+    {
+        if (SnapsEngine.GetDayOfWeekName() == "Saturday")
+        {
+            if (SnapsEngine.GetHourValue() > 8 * 60 + 15)
+                SnapsEngine.DisplayString("It is time to get up");
+        }
+        else
+        {
+            if (SnapsEngine.GetHourValue() > 6 * 60 + 15)
+                SnapsEngine.DisplayString("It is time to get up");
+        }
+    }
+}
