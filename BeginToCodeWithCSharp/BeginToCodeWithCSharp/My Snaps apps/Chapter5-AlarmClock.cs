a14a5c3e424f5911f9ec16f03b95f23896b1ac48
Dean Friedland <dfmmalaw@gmail.com>
2016-12-07 22:40:14 -0500

    built fortune teller app

--- BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-AlarmClock.cs
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-AlarmClock.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 class Chapter5_AlarmClock
 {
+    // this will test to see if the time is 9:15 or 6:15 by converting the hour to minutes
     public void StartProgram()
     {
         if (SnapsEngine.GetDayOfWeekName() == "Saturday")
---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-FortuneTeller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginToCodeWithCSharp.My_Snaps_apps
+{
+    class Chapter5_FortuneTeller
+    {
+    }
+}
