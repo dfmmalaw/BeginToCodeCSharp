f6c658d5b23b5ddc2195f71fdee902f04d7a878e
Dean Friedland <dfmmalaw@gmail.com>
2016-12-18 16:04:48 -0500

    Created time table app

--- BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-FortuneTeller.cs
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter5-FortuneTeller.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using SnapsLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BeginToCodeWithCSharp.My_Snaps_apps
+public class Chapter5_FortuneTeller
 {
-    class Chapter5_FortuneTeller
+    public void StartProgram()
     {
+        if (SnapsEngine.ThrowDice() < 4 || SnapsEngine.ThrowDice() > 8)
+        {
+            SnapsEngine.SpeakString("You are going to meet a tall, attractive stranger");
+        }        
+        else
+            SnapsEngine.SpeakString("You are not going to meet anyone at all");
     }
 }
---
+++ BeginToCodeWithCSharp/BeginToCodeWithCSharp/My Snaps apps/Chapter6-CountVehicleTypes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnapsLibrary;
+
+public class Chapter6_CountVehicleTypes
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Select Vehicle Type");
+
+        int carsCount = 0;
+        int vansCount = 0;
+        int trucksCount = 0;
+        int bikesCount = 0;
+
+        // repeatedly ask for pizza selections
+        while (true)
+        {
+            string vehicleType = SnapsEngine.SelectFrom5Buttons(
+                "Cars",
+                "Vans",
+                "Trucks",
+                "Bikes",
+                "Show Totals");
+
+            if (vehicleType == "Cars")
+                carsCount = carsCount + 1;
+
+            if (vehicleType == "Vans")
+                vansCount = vansCount + 1;
+
+            if (vehicleType == "Trucks")
+                trucksCount = trucksCount + 1;
+
+            if (vehicleType == "Bikes")
+                bikesCount = bikesCount + 1;
+
+            if (vehicleType == "Show Totals")
+            {
+                SnapsEngine.ClearTextDisplay();
+
+                SnapsEngine.AddLineToTextDisplay("Vehicle Totals");
+                SnapsEngine.AddLineToTextDisplay(carsCount.ToString() +
+                    " Cars");
+                SnapsEngine.AddLineToTextDisplay(vansCount.ToString() +
+                    " Vans");
+                SnapsEngine.AddLineToTextDisplay(trucksCount.ToString() +
+                    " Trucks");
+                SnapsEngine.AddLineToTextDisplay(bikesCount.ToString() +
+                    " Bikes");
+
+                string reply = SnapsEngine.SelectFrom2Buttons(item1: "Done",
+                    item2: "Reset");
+
+                if (reply == "Reset")
+                {
+                    carsCount = 0;
+                    vansCount = 0;
+                    trucksCount = 0;
+                    bikesCount = 0;
+                }
+                // clear the total display from the screen ready for more choices
+                SnapsEngine.ClearTextDisplay();
+            }
+        }
+    }
+}
